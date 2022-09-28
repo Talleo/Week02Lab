e0c107779f2e8170bba97e556f66789bcfa965a3
Leo Talbot <leomtalbot@gmail.com>
2022-09-28 16:35:12 -0600

    Created New Project A simple java application that will say hello if you're me

---
+++ src/hellotest/HelloTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellotest;
+
+/**
+ *
+ * @author talleo
+ */
+public class HelloTest {
+
+    public static void main(String[] args) {
+        boolean isCool = false;
+	if(isCool) 
+        {
+            System.out.println("Sup, world?");
+	}
+	else 
+        {
+            System.out.println("Hello world!");
+	}
+    }
+    
+}
